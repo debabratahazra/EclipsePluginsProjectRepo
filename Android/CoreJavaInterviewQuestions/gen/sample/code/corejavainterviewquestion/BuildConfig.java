16f942e8e9702aaee409e7aba6f918326d46250b
debabratahazra <debabrata.hazra@gmail.com>
2015-09-01 20:48:48 +0530

    remove android projects

--- Android/CoreJavaInterviewQuestions/gen/sample/code/corejavainterviewquestion/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package sample.code.corejavainterviewquestion;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
